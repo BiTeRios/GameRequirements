0e84f5fc5428624278bdb86140e9f626b11d778e
Виорел Варанице <vvaranice@gmail.com>
2025-09-08 03:16:37 +0300

    Реализована общая структура для регистрации и входа пользователя.

---
+++ GameRequirements.Bll/Interface/ISession.cs
@@ -0,0 +1,15 @@
+﻿using GameRequirements.Common.DTO.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRequirements.Bll.Interface
+{
+    public interface ISession
+    {
+        SingInSuccess SingIn(SingInDTO singInDTO);
+        LogInSuccess LogIn(LogInDTO logIndto);
+    }
+}
---
+++ GameRequirements.Common/DTO/Auth/LogInDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRequirements.Common.DTO.Auth
+{
+    public class LogInDTO
+    {
+        public string Email { get; set; } //Email
+        public string Password { get; set; } //Password
+        public string LoginIp { get; set; } //ID user
+        public DateTime DateTime { get; set; } //Entry time
+    }
+}
