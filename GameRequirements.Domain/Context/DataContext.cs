baf4a3297ebc2642e63d706800b91662c36616c7
Виорел Варанице <vvaranice@gmail.com>
2025-09-18 11:49:04 +0300

    Переделаны логика регистрации и входа, с использованием асинхроных методов

---
+++ GameRequirements.Domain/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using GameRequirements.Domain.Entites.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRequirements.Domain.Context
+{
+    public class DataContext : DbContext
+    {
+        //public UserConext() : base("name=") { }                        //Entry name DB later !!!!!!!!!!!!!!!!
+        public virtual DbSet<DBUser> Users { get; set; }
+        public virtual DbSet<DBUserSession> Sessions { get; set; }
+    }
+}
---
+++ GameRequirements.Domain/Entites/BaseId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRequirements.Domain.Entites
+{
+    public abstract class BaseId
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public required Guid Uuid { get; set; }
+
+        public bool IsActive = true;
+
+        public DateTime CreatedDate = DateTime.Now;
+    }
+}
