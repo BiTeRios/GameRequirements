c53245a305140951d3c0f22be95c302a283bb07c
Виорел Варанице <vvaranice@gmail.com>
2025-09-18 11:49:04 +0300

    Переделаны логика регистрации и входа, с использованием асинхроных методов

---
+++ GameRequirements.Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using GameRequirements.Bll;
+using GameRequirements.Bll.Interface;
+using GameRequirements.Common.DTO.Auth;
+using GameRequirements.Common.DTO.Responses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameRequirements.Api.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly ISessionBL _sessionBL;
+
+        public AuthController(ISessionBL sessionBL)
+        {
+            _sessionBL = sessionBL;
+        }
+
+        [Route("token")]
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn([FromBody] SingInDTO dto)
+        {
+            var result = await _sessionBL.SingIn(dto);
+            return Ok(result);
+        }
+    }
+}
---
+++ GameRequirements.Dal/Core/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using GameRequirements.Domain.Context;
+using GameRequirements.Domain.Entites.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRequirements.Dal.Core
+{
+    public class UserRepository(DataContext dataContext) : BaseRepository(dataContext)
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        /// <summary>
+        /// Проверяет, существует ли пользователь с таким email.
+        /// </summary>
+        public bool EmailExists(string email)
+        {
+            return _dataContext.Users.Any(u => u.Email == email);
+        }
+
+        /// <summary>
+        /// Получает пользователя по email.
+        /// </summary>
+        public DBUser? GetUserByEmail(string email)
+        {
+            return _dataContext.Users
+                .FirstOrDefault(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        /// Получает сессию по refresh token.
+        /// </summary>
+        public async Task<DBUserSession?> GetSessionByRefreshToken(string refreshToken)
+        {
+            var session = await _dataContext.Sessions.Include(x => x.User)
+                .FirstOrDefaultAsync(x => x.RefreshToken == refreshToken);
+
+            return session;
+        }
+    }
+}
